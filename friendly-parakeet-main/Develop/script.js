30ecca6d2c64f90ef2c0e79a0f2cf4ad7fe22c7e
Oscar Campos <oscarc691@gmail.com>
2023-01-15 15:58:03 -0800

    added some checkboxes

--- friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/Develop/script.js
@@ -1,5 +1,37 @@
 // Assignment code here
+const characterAmountRange = document.getElementById( 'characterAmountRange')
+const characterAmountNumber = document.getElementById( 'characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumberElement = document.getElementById('includeNumber')
+const includeSymbolsElement =document.getElementById('includeSymbols')
+const form =getElementById('checkBoxes')
 
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumber = includeNumberElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+
+  const password = generatePassword(characterAmount, includeUppercase, includeNumber, includeSymbols)
+})
+
+function generatePassword(characterAmount, includeUppercase, includeNumber, includeSymbols) {
+  String.fromCharCode(65)
+}
+
+
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
